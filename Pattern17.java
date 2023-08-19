1f37e41a2f893585a7f6e84b7aa60facfe268690
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-19 10:21:29 +0530

    Add files via upload

---
+++ Pattern17.java
@@ -0,0 +1,10 @@
+public class Pattern17 {
+    public static void main(String[] args) {
+        for(int i=65;i<=70;i++){
+            for(int j=65;j<=i;j++){
+                System.out.print((char)i);
+            }
+            System.out.println();  
+        }
+    }
+}
